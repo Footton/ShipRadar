068cd33b76399711e139fb99f4ac5ea610e4213a
Mose <nekronmoseley@gmail.com>
2016-01-23 17:35:31 +0000

    Fixed most errors in RadarCreation

--- src/Creation/RadarCreation.java
+++ src/Creation/RadarCreation.java
@@ -11,63 +11,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class RadarCreation extends JavaPlugin {
 	
+	final List<NewRadar> CURRENT_RADARS = new ArrayList<NewRadar>();
+	
 	public void onEnable(){
 		// Register PlayerListener
 		//registerEvents is giving me error
 		getPluginLoader().registerEvents(new PlayerListener(), this);
-		
-		HashSet<NewRadar> currentRadars = new HashSet<NewRadar>();
-		
 		Commands executor = new Commands();
 		//setExecutor is giving me error
 		this.getCommand("shipradar").setExecutor(executor);
 		
 
 	}
-	
-
-// error on saveDefaultConfig
-	private void saveDefaultConfig() {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-
-	private Object getCommand(String string) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-
-	private Object getConfig() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-
-	private Object getServer() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
 
 	public void onDisable(){
-		//error on both currentRadars and destroy
-		Object currentRadars;
-		for(NewRadar radar : currentRadars){
+		for(NewRadar radar : CURRENT_RADARS){
 			radar.destroy();
 		}
 	}
 
-	@Override
-	//error on run
-	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
-	
 }
